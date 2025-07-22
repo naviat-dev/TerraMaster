63791ef8309a8d87320579d03255df39124ae0ec
Israel Emmanuel <sriemmanuel787@outlook.com>
2025-07-21 21:38:20 -0700

    Adds WebMessageReceived handler for logging WebView messages
    Introduces a WebMessageReceived event handler to process messages
    from the WebView control. Logs JavaScript messages prefixed with
    "LOG:" as standard logs and those prefixed with "ERR:" as errors.
    
    Enhances debugging capabilities by providing insights into
    JavaScript activity within the WebView.

--- TerraMaster/TilePage.xaml.cs
+++ TerraMaster/TilePage.xaml.cs
@@ -2,11 +2,19 @@ namespace TerraMaster;
 
 public sealed partial class TilePage : Page
 {
-    public TilePage()
-    {
-        this.InitializeComponent();
-        _ = Cesium.StartAsync();
-        _ = MyWebView.EnsureCoreWebView2Async();
-        MyWebView.Source = new Uri(Cesium.Url + "/assets/cesium.js.html");
-    }
+	public TilePage()
+	{
+		this.InitializeComponent();
+		_ = Cesium.StartAsync();
+		_ = MyWebView.EnsureCoreWebView2Async();
+		MyWebView.Source = new Uri(Cesium.Url + "/assets/cesium.js.html");
+		MyWebView.WebMessageReceived += (sender, e) =>
+			{
+				string message = e.TryGetWebMessageAsString();
+				if (message.StartsWith("LOG:"))
+					Console.WriteLine("[JS LOG] " + message[4..]);
+				else if (message.StartsWith("ERR:"))
+					Console.WriteLine("[JS ERR] " + message[4..]);
+			};
+	}
 }
