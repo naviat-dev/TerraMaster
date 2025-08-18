a8c7791b1dca61196d8551abc49176e5d2537aa5
Israel Emmanuel <sriemmanuel787@outlook.com>
2025-08-18 15:40:04 -0700

    Simplifies WebMessageReceived handler for logging
    Replaces conditional checks for specific message prefixes with a simplified
    logging approach in the WebMessageReceived event handler. Improves code
    readability and reduces redundancy while maintaining functionality.

--- TerraMaster/TilePage.xaml.cs
+++ TerraMaster/TilePage.xaml.cs
@@ -8,13 +8,10 @@ public sealed partial class TilePage : Page
 		_ = Cesium.StartAsync();
 		_ = MyWebView.EnsureCoreWebView2Async();
 		MyWebView.Source = new Uri(Cesium.Url + "/assets/cesium.js.html");
-		MyWebView.WebMessageReceived += (sender, e) =>
-			{
-				string message = e.TryGetWebMessageAsString();
-				if (message.StartsWith("LOG:"))
-					Console.WriteLine("[JS LOG] " + message[4..]);
-				else if (message.StartsWith("ERR:"))
-					Console.WriteLine("[JS ERR] " + message[4..]);
-			};
+		MyWebView.WebMessageReceived += (s, e) =>
+		{
+			var message = e.TryGetWebMessageAsString();
+			Console.WriteLine("[JS: " + message + "]");
+		};
 	}
 }
