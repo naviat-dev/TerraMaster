0bb94c843b450d839ec31c04d205e1f03f994e56
Israel Emmanuel <sriemmanuel787@outlook.com>
2025-09-18 21:46:22 -0700

    Implemented LOD with tile display

--- TerraMaster/Assets/driver.js
+++ TerraMaster/Assets/driver.js
@@ -19,7 +19,7 @@ window.onload = async () => {
 	});
 
 	const largeTilesDS = new Cesium.CustomDataSource("largeTiles");
-	const smallTilesDS = new Cesium.CustomDataSource("smallTiles");
+	const smallTilesGrp = [];
 
 	// viewer.dataSources.add(smallTilesDS);
 	viewer.dataSources.add(largeTilesDS);
@@ -27,6 +27,7 @@ window.onload = async () => {
 		for (var j = -180; j < 180; j += 10) {
 			largeTilesDS.entities.add({
 				name: (i + 5) + "°, " + (j + 5) + "°",
+				id: "lTile" + (i + 90) + "-" + (j + 180),
 				rectangle: {
 					coordinates: Cesium.Rectangle.fromDegrees(j, i, j + 10, i + 10),
 					material: Cesium.Color.WHITE.withAlpha(0.01), // Invisible but clickable
@@ -45,15 +46,20 @@ window.onload = async () => {
 	// Instead, squares are grouped by bounding box and added/removed from the render as the viewbox changes
 	for (var i = -90; i < 90; i++) {
 		var tileWidth = 0;
+		var grpYInd = Math.floor(i / 10) + 9;
 		for (var j = 0; j < latitudeIndex.length; j++) {
 			if (Math.abs(i) >= latitudeIndex[j][0]) {
 				tileWidth = latitudeIndex[j][1];
 				break;
 			}
 		}
+		if (smallTilesGrp.length <= grpYInd) smallTilesGrp.push([]);
 		for (var j = -180; j < 180; j += tileWidth) {
-			smallTilesDS.entities.add({
+			var grpXInd = Math.abs(i) >= 89 ? Math.floor(j / 12) + 15 : Math.floor(j / 10) + 18; // 12° at poles, 10° elsewhere
+			if (smallTilesGrp[grpYInd].length <= grpXInd) smallTilesGrp[grpYInd].push(new Cesium.CustomDataSource("smallTiles" + grpYInd + "-" + grpXInd));
+			smallTilesGrp[grpYInd][grpXInd].entities.add({
 				name: (i + 0.5) + "°, " + (j + (tileWidth / 2)) + "°",
+				id: "sTile" + (i + 90) + "-" + (j + 180),
 				rectangle: {
 					coordinates: Cesium.Rectangle.fromDegrees(j, i, j + tileWidth, i + 1),
 					material: Cesium.Color.WHITE.withAlpha(0.01), // Invisible but clickable
@@ -69,10 +75,9 @@ window.onload = async () => {
 	}
 
 	viewer.scene.globe.show = true;
-	var viewbox;
-	var camPos;
-	var alt;
+	var viewbox, camPos, alt;
 	var tileSize = 1; // 0 is small, 1 is large
+	var boundsNPrev = 0, boundsSPrev = 0, boundsEPrev = 0, boundsWPrev = 0;
 
 	// Runs every frame. Most drawing code should go here.
 	viewer.scene.postRender.addEventListener(() => {
@@ -80,51 +85,46 @@ window.onload = async () => {
 		camPos = viewer.scene.camera.positionCartographic;
 		alt = camPos.height;
 		if (viewbox) {
-			if (alt < 2000000) {
-				// Show smaller tiles and hide larger tiles
-				if (tileSize) {
-					tileSize = 0;
-					// smallTilesDS.show = true;
-					largeTilesDS.show = false;
-				}
-			} else {
-				// Hide smaller tiles and show larger tiles
-				if (!tileSize) {
-					tileSize = 1;
-					// smallTilesDS.show = false;
-					largeTilesDS.show = true;
+			// These are the values that are actually used to determine if the viewbox has changed.
+			// The viewbox can still move around, but should only trigger when tiles need to be added/removed.
+			var boundsNCur = Math.floor(Cesium.Math.toDegrees(viewbox.north) / 10) + 9;
+			var boundsSCur = Math.floor(Cesium.Math.toDegrees(viewbox.south) / 10) + 9;
+			var boundsECur = Math.floor(Cesium.Math.toDegrees(viewbox.east) / 10) + 18;
+			var boundsWCur = Math.floor(Cesium.Math.toDegrees(viewbox.west) / 10) + 18;
+			// If the viewbox has changed, add/remove tiles as necessary
+			if (boundsNCur !== boundsNPrev || boundsSCur !== boundsSPrev || boundsECur !== boundsEPrev || boundsWCur !== boundsWPrev) {
+				console.log("Viewbox changed. Altitude: " + alt.toFixed(0) + "m, Bounds: N" + (boundsNCur - 9) * 10 + "° S" + (boundsSCur - 9) * 10 + "° E" + (boundsECur - 18) * 10 + "° W" + (boundsWCur - 18) * 10 + "°");
+				console.log("Old viewbox: N" + (boundsNPrev - 9) * 10 + "° S" + (boundsSPrev - 9) * 10 + "° E" + (boundsEPrev - 18) * 10 + "° W" + (boundsWPrev - 18) * 10 + "°");
+				boundsNPrev = boundsNCur;
+				boundsSPrev = boundsSCur;
+				boundsEPrev = boundsECur;
+				boundsWPrev = boundsWCur;
+				if (alt < 2000000) {
+					// Show smaller tiles and hide larger tiles
+					if (tileSize) {
+						tileSize = 0;
+						viewer.dataSources.removeAll();
+					}
+					for (var i = boundsSCur; i <= boundsNCur; i++) {
+						for (var j = boundsWCur; j <= boundsECur; j++) {
+							console.log("there's a chance you get stuck here...");
+							if (i < smallTilesGrp.length && j < smallTilesGrp[i].length) {
+								console.log("...so let's see");
+								if (!viewer.dataSources.contains(smallTilesGrp[i][j])) {
+									viewer.dataSources.add(smallTilesGrp[i][j]);
+								}
+							}
+						}
+					}
+				} else {
+					// Hide smaller tiles and show larger tiles
+					if (!tileSize) {
+						tileSize = 1;
+						viewer.dataSources.removeAll();
+						viewer.dataSources.add(largeTilesDS);
+					}
 				}
 			}
 		}
-
-		console.log("Camera Position:" + camPos);
-		console.log("Altitude:" + alt);
-		// console.log("Viewbox:" + viewbox.west + "," + viewbox.south + "," + viewbox.east + "," + viewbox.north);
 	});
-
-
-
-	/* This will draw every terrain tile. It should be noted that drawing every tile takes up bucketloads of resources, and should not be run in its current state.
-	Once paired with viewboxes, this will be more useful.
-	for (var i = -90; i < 90; i++) {
-		var tileWidth = 0;
-		for (var j = 0; j < latitudeIndex.length; j++) {
-			if (Math.abs(i) >= latitudeIndex[j][0]) {
-				tileWidth = latitudeIndex[j][1];
-				break;
-			}
-		}
-		for (var j = -180; j < 180; j += tileWidth) {
-			viewer.entities.add({
-				rectangle: {
-					coordinates: Cesium.Rectangle.fromDegrees(j, i, j + tileWidth, i + 1),
-					material: Cesium.Color.BLUE.withAlpha(0.4),
-					height: 0, // <-- this disables terrain clamping
-					outline: true,
-					outlineColor: Cesium.Color.GRAY,
-					outlineWidth: 2
-				}
-			});
-		}
-	} */
 };
