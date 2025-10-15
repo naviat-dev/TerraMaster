5ec3e6f36b4ee6f3a93959019e55019b6c81eab0
naviat-dev <sriemmanuel787@outlook.com>
2025-10-15 08:00:13 -0700

    Adds support for Cesium token and server URLs config
    Introduces configuration options for Cesium authentication and server URLs
    to enable integration with external services and centralized endpoint management.

--- TerraMaster/Config.cs
+++ TerraMaster/Config.cs
@@ -5,4 +5,6 @@ public class Config
 	static string _savePath;
 	static int _queueSize = 20;
 	static int _orthoRes = 2048;
+	static string _cesiumToken = "";
+	static string[] _serverUrls = Util.Ws2ServerUrls;
 }
