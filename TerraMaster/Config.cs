3dbe83d5cdf52fa1264745af0c3f93058aafe529
naviat-dev <sriemmanuel787@outlook.com>
2025-10-19 12:24:02 -0700

    Makes config fields mutable and adds tile border color
    Allows configuration fields to be changed at runtime, improving flexibility for dynamic settings adjustments.
    Introduces a new field for tile border color to support future customization.
    Suppresses IDE warning to permit non-readonly fields.

--- TerraMaster/Config.cs
+++ TerraMaster/Config.cs
@@ -2,9 +2,12 @@ namespace TerraMaster;
 
 public class Config
 {
-	private static readonly string _savePath;
-	private static readonly int _queueSize = 20;
-	private static readonly int _orthoRes = 2048;
-	private static readonly string _cesiumToken = "";
-	private static readonly string[] _serverUrls = Util.Ws2ServerUrls;
+	#pragma warning disable IDE0044
+	private static string _savePath = "";
+	private static int _queueSize = 20;
+	private static int _orthoRes = 2048;
+	private static string _cesiumToken = "";
+	private static string[] _serverUrls = Util.Ws2ServerUrls;
+	private static string _tileBorderColor = "";
+	#pragma warning restore IDE0044
 }
